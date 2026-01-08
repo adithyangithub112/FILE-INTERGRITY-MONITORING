ef6f415d2bb694aefd15a4183144ddd9e516326e
Adithyan Suresh <adithyansuresh161@gmail.com>
2026-01-08 10:36:10 +0530

    Update email_alert.py

--- fim-project/email_alert.py
+++ fim-project/email_alert.py
@@ -1,5 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
+from datetime import datetime
 import os
 
 SMTP_SERVER = "smtp.gmail.com"
@@ -10,21 +11,20 @@ PASSWORD = os.getenv("ALERT_EMAIL_PASSWORD")
 RECEIVER = os.getenv("ALERT_EMAIL_RECEIVER")
 
 
-def send_email(file_path, old_hash, new_hash):
-    body = f"""
-File Integrity Alert
-
-File: {file_path}
-Old Hash: {old_hash}
-New Hash: {new_hash}
-"""
+def send_email(subject, body):
+    if not all([SENDER, PASSWORD, RECEIVER]):
+        print("[ERROR] Email environment variables not set")
+        return
 
     msg = MIMEText(body)
-    msg["Subject"] = "🚨 File Integrity Alert"
+    msg["Subject"] = subject
     msg["From"] = SENDER
     msg["To"] = RECEIVER
 
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-        server.starttls()
-        server.login(SENDER, PASSWORD)
-        server.send_message(msg)
+    try:
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(SENDER, PASSWORD)
+            server.send_message(msg)
+    except Exception as e:
+        print(f"[ERROR] Email sending failed: {e}")
